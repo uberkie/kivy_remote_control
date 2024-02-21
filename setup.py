f3d5a720c02e4ffe2b7cf34af975c3ebc4b9c0df
uberkie <44478849+uberkie@users.noreply.github.com>
2024-02-21 10:20:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,21 @@
 import io
 import re
-from glob import glob
-from os.path import basename
-from os.path import dirname
-from os.path import join
-from os.path import splitext
-
-from setuptools import find_packages
-from setuptools import setup
-
+from os.path import dirname, join
+from setuptools import find_packages, setup
 
 def read(*names, **kwargs):
     with io.open(join(dirname(__file__), *names), encoding=kwargs.get('encoding', 'utf8')) as fh:
         return fh.read()
 
+# Remove badges from README content
+README_content = read('README.rst')
+long_description = re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', README_content)
+
 setup(
     name='kivy_remote_control',
-    version='0.1', 
+    version='0.0.0.1', 
     description='Kivy android joystick for mavsdk',
-    long_description='{}\n{}'.format(
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
-    ),
+    long_description=long_description,
     packages=find_packages(where="src"),
     package_dir={'': 'src'},
     python_requires=">3.6, <=3.11",
@@ -38,4 +33,6 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
+    license='MIT',  # Add license information
+    author='uberkie',  # Add author information
 )
