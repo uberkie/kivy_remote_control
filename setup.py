e8845978edabaa749940d4d6b79739355ba17d5e
uberkie <44478849+uberkie@users.noreply.github.com>
2024-02-21 09:39:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import pathlib
 here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
-long_description = (here / "README.md").read_text(encoding="utf-8")
+long_description = ("README.md").read_text(encoding="utf-8")
 setup(
     name='kivy_remote_control',
     
