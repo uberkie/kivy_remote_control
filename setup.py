153e5d998764f2e21f52b63bce7873a09e53f388
uberkie <aj@datainc.co.za>
2024-02-21 08:48:12 +0200

    `

--- setup.py
+++ setup.py
@@ -1,32 +1,16 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+import pathlib
 
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
 setup(
     name='kivy_remote_control',
-    packages=['kivy_remote_control'],
+    
     version='0.1', 
-    license='MIT',
+    
     description='Kivy android joystick for mavsdk',
-    author='aj',
-    author_email='aj@datainc.co.za',
-    url='https://github.com/uberkie/kivy_remote_control',
-    download_url='https://github.com/user/kivy_remote_control/archive/v_01.tar.gz',
-    keywords=['kivy', 'mavlink', 'mavsdk', 'joystick', 'android'],
-    install_requires=[
-        'kivy',
-
-    ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-
-
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
+    packages=find_packages(where="src"),
+    python_requires=">3.6, <=3.11",
 )
