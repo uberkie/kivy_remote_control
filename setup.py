9c75b508b913f5c06f788a5e4b447ec060d40436
uberkie <44478849+uberkie@users.noreply.github.com>
2024-02-21 09:41:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import pathlib
 here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
-long_description = ("README.md").read_text(encoding="utf-8")
+long_description = 'Kivy android joystick for mavsdk'
 setup(
     name='kivy_remote_control',
     
